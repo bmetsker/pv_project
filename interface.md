115c1847443496f12d7fbad9a0556a318e1e1d7b
Ben <benmetsker@gmail.com>
2020-06-09 16:25:19 -0700

    6/9/20, 4pm

--- interface.md
+++ interface.md
@@ -1,1 +1,4 @@
-'Hello, World'
+# Let's See If I Still Know How To Do This
+## Here we go...
+I really hope this worked
+
--- script.R
+++ script.R
@@ -1,1 +1,9 @@
-'please God work
+'please God work'
+library(dplyr)
+
+
+library(httr)
+apiKey <- "33kz-ixgy"
+result <- GET("https://data.seattle.gov/resource/33kz-ixgy.json",
+              add_headers(Authorization = paste("Key", apiKey)))
+
